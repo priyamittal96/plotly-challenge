d78486758ed0c10cf0f70959f07e4ebbd1ace190
shantipriya30 <75239249+shantipriya30@users.noreply.github.com>
2021-04-03 17:26:51 -0700

    Bubble Chart

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -10,6 +10,32 @@ function buildCharts(sample){
         })
         var result = resultsArray[0];
 
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+    
+        // Build Bubble Chart
+        var bubbleLayout ={
+            title: "Bacteria Cultures Per Sample",
+            hovermode: "closest", 
+            xaxis: {title: "OTU ID"},
+            margin: {t: 30}
+        }
+        var bubbleData = [{
+            x: otu_ids,
+            y: sample_values,
+            text: otu_labels,
+            mode: "markers", 
+            marker: {
+                size: sample_values,
+                color: otu_ids,
+                colorscale: "Earth"
+            }
+
+        }]
+
+        Plotly.newPlot("bubble", bubbleData, bubbleLayout)
+
     })
 }
 
